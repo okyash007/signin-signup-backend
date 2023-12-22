a0a8382c1ccbe75804f9118d14a847926dabb0ee
okyash007 <helloyashverma@gmail.com>
2023-12-22 20:11:55 +0530

    fixed errrors

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(process.env.MONGO)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
-    }
-}
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGO}`);
+    console.log(
+      `MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
