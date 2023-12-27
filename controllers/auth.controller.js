3d751de83836fef1c5937d155accb287e28452c5
okyash007 <helloyashverma@gmail.com>
2023-12-28 01:07:06 +0530

    trying httponly false

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const signin = async (req, res, next) => {
 
     const { password: pass, ...rest } = validUser._doc;
 
-    res.status(200).cookie("acess_token", token, { httpOnly: true }).json(rest);
+    res.status(200).cookie("acess_token", token, { httpOnly: false }).json(rest);
   } catch (error) {
     next(error);
   }
